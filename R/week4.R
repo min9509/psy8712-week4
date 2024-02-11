931e52f5daf413843cfeb32d89a518fe9b98108b
minjeong seo <minjeongseo@infra04-wg012.eduroam.wireless.umn.edu>
2024-02-11 16:46:05 -0600

    Remove these cases from wide_tbl. (Line12)

--- R/week4.R
+++ R/week4.R
@@ -4,13 +4,12 @@ library(tidyverse)
 
 # Data Import
 import_tbl <- read_delim(file = "../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
-str(import_tbl) # summary? glimpse??
+summary(import_tbl) # str? glimpse??
 wide_tbl <- separate(import_tbl, col = "qs", into = c("q1", "q2", "q3", "q4", "q5"))
 wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
 wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%Y %m %d, %H:%M:%S") 
-wide_tbl[,5:9] 
-
+wide_tbl[,5:9][wide_tbl[,5:9] == 0] <- NA
+wide_tbl <- drop_na(wide_tbl, q2)
 wide_tbl
-?as.POSIXct
-
-?replace_na
+?drop_na
+?mutate()
