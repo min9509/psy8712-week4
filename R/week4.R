d06c09768dce2d553e376968b312c2151a7e73f2
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-02-10 21:54:55 -0600

    Convert the datadate column into a class appropriate for datetimes (Line 10)

--- R/week4.R
+++ R/week4.R
@@ -6,5 +6,9 @@ library(tidyverse)
 import_tbl <- read_delim(file = "../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 str(import_tbl) # summary? glimpse??
 wide_tbl <- separate(import_tbl, col = "qs", into = c("q1", "q2", "q3", "q4", "q5"))
+wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
 wide_tbl
-?separate
+wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%Y %m %d, %H:%M:%S") 
+
+wide_tbl
+?as.POSIXct
