335d1f8e00568ae4d1cd4b5a9558257d94c07db6
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-02-10 21:25:12 -0600

    Save a new variable called wide_tbl (Line 8)

--- R/week4.R
+++ R/week4.R
@@ -5,3 +5,6 @@ library(tidyverse)
 # Data Import
 import_tbl <- read_delim(file = "../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
 str(import_tbl) # summary? glimpse??
+wide_tbl <- separate(import_tbl, col = "qs", into = c("q1", "q2", "q3", "q4", "q5"))
+wide_tbl
+?separate
