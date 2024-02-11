9a12370f6539d93ec3f4017212832c069f1bde88
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-02-10 22:13:03 -0600

    Try to solve line 11

--- R/week4.R
+++ R/week4.R
@@ -7,8 +7,10 @@ import_tbl <- read_delim(file = "../data/week4.dat", delim = "-", col_names = c(
 str(import_tbl) # summary? glimpse??
 wide_tbl <- separate(import_tbl, col = "qs", into = c("q1", "q2", "q3", "q4", "q5"))
 wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
-wide_tbl
 wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%Y %m %d, %H:%M:%S") 
+wide_tbl[,5:9] 
 
 wide_tbl
 ?as.POSIXct
+
+?replace_na
