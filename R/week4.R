2f652191e2945a9e6220e52972a5c3d2c93fc642
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-02-10 20:39:24 -0600

    Import week4.dat as import_tbl (Line 6)

--- R/week4.R
+++ R/week4.R
@@ -1,3 +1,7 @@
 # Script Settings and Resources
 setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
 library(tidyverse)
+
+# Data Import
+import_tbl <- read_delim(file = "../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
+?tidyverse
