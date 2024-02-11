19f090d4428fc20a0762903d2d40c2892aa7a2d6
minjeong seo <minjeongseo@minjeongui-MacBookAir.local>
2024-02-10 20:46:34 -0600

    Summary information about the data frame (Line 7)

--- R/week4.R
+++ R/week4.R
@@ -4,4 +4,4 @@ library(tidyverse)
 
 # Data Import
 import_tbl <- read_delim(file = "../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
-?tidyverse
+str(import_tbl) # summary? glimpse??
