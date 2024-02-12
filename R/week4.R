9faf8950448640f693a57e2177cf8ac4d81b0ea1
minjeong seo <minjeongseo@infra04-wg012.eduroam.wireless.umn.edu>
2024-02-12 13:58:43 -0600

    Notetaking in the class session.

--- R/week4.R
+++ R/week4.R
@@ -4,10 +4,10 @@ library(tidyverse)
 
 # Data Import
 import_tbl <- read_delim(file = "../data/week4.dat", delim = "-", col_names = c("casenum", "parnum", "stimver", "datadate", "qs"))
-summary(import_tbl) # or str or glimpse?
+glimpse(import_tbl) # The correct answer is the glimpse function rather than the summary function
 wide_tbl <- separate(import_tbl, col = "qs", into = c("q1", "q2", "q3", "q4", "q5"))
-wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer)
-wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%Y %m %d, %H:%M:%S") # I am not sure why it doesn't work..
-wide_tbl[,5:9][wide_tbl[,5:9] == 0] <- NA
-wide_tbl <- drop_na(wide_tbl, q2)
+wide_tbl[,5:9] <- sapply(wide_tbl[,5:9], as.integer) # It is not a best practice. 
+wide_tbl$datadate <- as.POSIXct(wide_tbl$datadate, format = "%b %d %Y, %H:%M:%S") # %b and %B (month names)
+wide_tbl[,5:9][wide_tbl[,5:9] == 0] <- NA # Checking alternative ways.
+wide_tbl <- drop_na(wide_tbl, q2) # naming consistency is important! 
 long_tbl <- pivot_longer(wide_tbl, cols = q1:q5, names_to = "question", values_to = "response")
